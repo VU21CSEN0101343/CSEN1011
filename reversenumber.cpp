cef55a6f2cfbf9050664e8bf33a041e2ba2cc042
manikrishnajaladi <93367811+manikrishnajaladi@users.noreply.github.com>
2021-12-14 14:26:50 +0530

    Add files via upload

---
+++ reversenumber.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a,b=0,c;
+	printf("enter a number:");
+	scanf("%d",&a);
+	while(a!=0)
+	{
+		c=a%10;
+		b=b*10+c;
+		a=a/10;
+	}
+	printf("reverse of a number:%d",b);
+	
+}
